f517817fa8ce9a09b14b7dafb60c2af67c7fbd0a
Jon Stump <jmstump@gmail.com>
2021-03-29 11:40:24 -0700

    add messages for creating and updating reviews
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,13 +11,15 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.create(review_params)
-    json_response(@review)
+    @review = Review.create!(review_params)
+    json_response(@review, :created)
   end
 
   def update
     @review = Review.find(params[:id])
-    @review.update(review_params)
+    if @review.update!(review_params)
+      render status: 200, json: { message: "This review has been updated successfully"}
+    end
   end
 
   def destroy
@@ -26,10 +28,6 @@ class ReviewsController < ApplicationController
   end
 
 private
-  def json_response(object, status = :ok)
-    render json: object, status: status
-  end
-
   def review_params
     params.permit(:username, :title, :content, :location_city, :location_country, :rating)
   end
