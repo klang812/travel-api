4fa78db32475e6792c2a254e70fedb7f2cb9db37
Jon Stump <jmstump@gmail.com>
2021-03-30 09:17:41 -0700

    get repo to current state on review.rb model
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,7 +8,7 @@ class Review < ApplicationRecord
   validates :location_country, presence: true
   validates_inclusion_of :rating, in: 1..5
 
-  scope :search_by_username, -> (username) { where username: username }
+  scope :search_by_username, ->  { where username: username }
   scope :search_by_title, -> (title) { where title: title }
   scope :search_by_location_city, -> (location_city) { where location_city: location_city }
   scope :search_by_location_country, -> (location_country) { where location_country: location_country }
@@ -16,3 +16,8 @@ class Review < ApplicationRecord
 end
 
 
+# ILIKE%
+
+# returned_projects = DB.exec("SELECT * FROM projects WHERE title ILIKE '%#{title}%';")
+
+# Book.where("location_city ILIKE ?", "%" + params[:location_city] + "%")
